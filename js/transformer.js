3a0cf5afaeb619683a88cc394ffb2c7b15d06e2c
Andrei Miclaus <miclaus.andrei@gmail.com>
2015-12-15 18:22:40 +0100

    upadted code for the xsl transformation and ajax requesting

--- js/transformer.js
+++ js/transformer.js
@@ -1,41 +1,39 @@
 /** 
-Code from http://www.w3schools.com/xsl/xsl_client.asp 
-Copyright 1999-2015 by Refsnes Data. All Rights Reserved.
+Inspired by http://www.w3schools.com/xsl/xsl_client.asp 
 **/
-function loadXMLDoc(filename)
-{
-  if (window.ActiveXObject)
-    {
-    xhttp = new ActiveXObject("Msxml2.XMLHTTP");
-    }
-  else 
-    {
+function ajaxRequest(method, url, responseType) {
+  if (window.ActiveXObject) {
+    if(responseType == "msxml-document")
+      responseType = "Msxml2.XMLHTTP"
+    xhttp = new ActiveXObject(responseType);
+  } else {
     xhttp = new XMLHttpRequest();
-    }
-  xhttp.open("GET", filename, false);
-  try {xhttp.responseType = "msxml-document"} catch(err) {} // Helping IE11
+  }
+  xhttp.open(method, url, false);
+  try {
+    xhttp.responseType = "msxml-document"
+  } catch (err) {} // IE11 catch
   xhttp.send("");
   return xhttp.responseXML;
 }
 
-function transform(xmlfile, xslfile)
-{
-  xml = loadXMLDoc(xmlfile);
-  xsl = loadXMLDoc(xslfile);
-  // code for IE
-  if (window.ActiveXObject || xhttp.responseType == "msxml-document")
-    {
+function transform(xmlfile, xslfile) {
+  xml = ajaxRequest("GET", xmlfile, "msxml-document");
+  xsl = ajaxRequest("GET", xslfile, "msxml-document");
+  // IE specific code
+  if (window.ActiveXObject || xhttp.responseType == "msxml-document") {
     ex = xml.transformNode(xsl);
     return ex;
-    // document.getElementById("results").innerHTML = ex;
-    }
+  }
   // code for Chrome, Firefox, Opera, etc.
-  else if (document.implementation && document.implementation.createDocument)
-    {
+  else if (documentImplementationAvailable()) {
     xsltProcessor = new XSLTProcessor();
     xsltProcessor.importStylesheet(xsl);
     resultDocument = xsltProcessor.transformToFragment(xml, document);
     return resultDocument;
-    // document.getElementById("results").appendChild(resultDocument);
-    }
+  }
+}
+
+function documentImplementationAvailable(){
+  return document.implementation && document.implementation.createDocument;
 }
